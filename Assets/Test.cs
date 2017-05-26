f1996ec379c97ce0bb964ecc807df06ab3e1121b
YanWenshu <yanws@gamebegin.com>
2017-05-26 10:50:58 +0800

    speedhack
    speedhack

---
+++ Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("time = " + Time.deltaTime);
+
+		Debug.Log ("test frame = " + Application.targetFrameRate);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		Debug.Log ("time = " + Time.deltaTime);
+	}
+}
---
+++ Assets/UnityDetector-master/README.md
@@ -0,0 +1,3 @@
+These unity scripts are about anti-speedhack and anti-debug.
+
+这些unity 脚本是非常初级的反加速和反调试脚本，看上去简单但是总是有效，注意和加密配合哦。
